ff30e43d2eef3e0ba1192d59bf0b14675e8fb1b7
Acolytetech <slwaniya03@gmail.com>
2024-11-29 10:32:33 +0530

    add some category

--- schemaTypes/post.js
+++ schemaTypes/post.js
@@ -1,4 +1,4 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export const post = defineType({
   name: 'post',
@@ -6,25 +6,43 @@ export const post = defineType({
   type: 'document',
   fields: [
     defineField({
-  
-        name: 'category',
-        title: 'Category',
-        type: 'string',
-        options: {
-          list: [
-            {title:'Latest' , value:'breaking news'},
-            { title: 'Education', value: 'education' },
-            {title:'Accident' , value:'accident'},
-            { title: 'Technology', value: 'technology' },
-            { title: 'Health', value: 'health' },
-            { title: 'Sports', value: 'sports' },
-            { title: 'Business', value: 'business' },
-            { title: 'Entertainment', value: 'entertainment' },
 
-          ],
-      
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Latest', value: 'breaking news' },
+          { title: 'Education', value: 'education' },
+          { title: 'Accident', value: 'accident' },
+          { title: 'Technology', value: 'technology' },
+          { title: 'Health', value: 'health' },
+          { title: 'Sports', value: 'sports' },
+          { title: 'Business', value: 'business' },
+          { title: 'Entertainment', value: 'entertainment' },
+          { title: 'Politics', value: 'politics' },
+          { title: 'Environment', value: 'environment' },
+          { title: 'Travel', value: 'travel' },
+          { title: 'Food', value: 'food' },
+          { title: 'Fashion', value: 'fashion' },
+          { title: 'Home', value: 'home' },
+          { title: 'Lifestyle', value: 'lifestyle' },
+          { title: 'Automotive', value: 'automotive' },
+          { title: 'Real Estate', value: 'real estate' },
+          { title: 'Finance', value: 'finance' },
+          { title: 'Art', value: 'art' },
+          { title: 'Music', value: 'music' },
+          { title: 'Film', value: 'film' },
+          { title: 'Theater', value: 'theater' },
+          { title: 'Sci/Tech', value: 'sci-tech' },
+          { title: 'World', value: 'world' },
+          { title: 'U.S.', value: 'us' },
+          { title: 'Asia', value: 'asia' },
+
+        ],
+
       },
-       
+
     }),
     defineField({
       name: 'title',
@@ -34,7 +52,7 @@ export const post = defineType({
     defineField({
       name: 'slug',
       type: 'slug',
-    
+
     }),
     defineField({
       name: 'publishedAt',
@@ -49,7 +67,7 @@ export const post = defineType({
     defineField({
       name: 'body',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{ type: 'block' }],
     }),
   ],
 })
